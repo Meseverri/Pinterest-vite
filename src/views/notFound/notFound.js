cb3f75ba874137b6d10a32ab87e0c76418873c2c
Meseverri <meseverrimarquez5@gmail.com>
2024-04-16 08:55:02 +0200

    gitIgnore+dist

---
+++ src/views/notFound/notFound.js
@@ -0,0 +1,8 @@
+import "/notFound.css"
+
+const notFound=(input)=>{
+    return `<div class="notFound">
+    <h2>No se encontraron resultados para:${input}</h2>
+    <p>Prueba haciendo otra busqueda</p>
+  </div>`
+}
